b2a9b40df502b0762d85fa6fe8ee9590a056aa4e
vipin2060 <vipinvikraman86@gmail.com>
2026-01-31 00:07:20 +0800

    Add outputs for DualMetro-Parent module

---
+++ outputs.tf
@@ -0,0 +1,32 @@
+
+output "Dx_connection_Name_Pri_Parent" {
+  value = module.Deploy-DualNE2AWS-DualMetro-Parent.Dx_connection_Name_Pri
+}
+
+output "Dx_connection_Name_Sec_Parent" {
+  value = module.Deploy-DualNE2AWS-DualMetro-Parent.Dx_connection_Name_Sec
+}
+
+output "Dx_connection_ID_Pri_Parent" {
+  value = module.Deploy-DualNE2AWS-DualMetro-Parent.Dx_connection_ID_Pri
+}
+
+output "Dx_connection_ID_Sec_Parent" {
+  value = module.Deploy-DualNE2AWS-DualMetro-Parent.Dx_connection_ID_Sec
+}
+
+output "Dx_VLAN_ID_Pri_Parent" {
+  value = module.Deploy-DualNE2AWS-DualMetro-Parent.Dx_VLAN_ID_Pri
+}
+
+output "Dx_VLAN_ID_Sec_Parent" {
+  value = module.Deploy-DualNE2AWS-DualMetro-Parent.Dx_VLAN_ID_Sec
+}
+
+output "Equinix_VC_ID_Pri_Parent" {
+  value = module.Deploy-DualNE2AWS-DualMetro-Parent.Equinix_VC_ID_Pri
+}
+
+output "Equinix_VC_ID_Sec_Parent" {
+  value = module.Deploy-DualNE2AWS-DualMetro-Parent.Equinix_VC_ID_Sec
+}
